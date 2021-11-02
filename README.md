124754acd15d6282ea1b5bc982099edcd35e2a78
claudiorodrigo <claudiorodrigozh@gmail.com>
2021-11-01 21:45:56 -0300

    refactor: update script start.sh

--- README.md
+++
@@ -1,2 +0,0 @@
-# start-project-node.sh
-Projeto com um arquivo de Script para baixar um ambiente de desenvolvimento com Typescript, Eslint, e padroões de commit
---
+++ start.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+init_project() {
+    yarn init -y
+}
+
+init_repo_git() {
+    git init
+}
+
+install_typescript() {
+    yarn add typescript -D
+    yarn add ts-node-dev -D
+    yarn tsc --init
+}
+
+install_package_for_update_package_json() {
+    yarn add npe --global
+}
+
+install_plugins_pattern_commits() {
+    yarn add @commitlint/config-conventional @commitlint/cli -D
+    echo "module.exports = { extends: ['@commitlint/config-conventional'] };" >commitlint.config.js
+
+    yarn add husky --dev
+    yarn husky install
+    yarn husky add .husky/commit-msg 'yarn commitlint --edit $1'
+    yarn add commitizen -D
+    yarn commitizen init cz-conventional-changelog --yarn --dev --exact
+
+    yarn npe script.commit "git-cz"
+}
+
+install_eslint() {
+    yarn add eslint@^7.2.0 -D
+    yarn add eslint-plugin-import@^2.22.1 -D
+    yarn add @typescript-eslint/parser@^5.0.0 -D
+    yarn add @typescript-eslint/eslint-plugin -D
+    yarn add eslint-config-airbnb-base@14.2.1 -D
+    yarn add eslint-import-resolver-typescript@2.5.0 -D
+    yarn add eslint-import-resolver-typescript@2.5.0 -D
+    yarn eslint --init
+}
+
+init_project
+init_repo_git
+install_typescript
+install_package_for_update_package_json
+install_plugins_pattern_commits
+install_eslint
+
